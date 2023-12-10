a9fbcef42ee252cc01acd9cc1eacb6b760e83505
Rafael <rafaelmagalhaes1@live.com>
2023-12-10 19:54:31 -0300

    feat: create connection.js file config db

---
+++ backend/src/db/connection.js
@@ -0,0 +1,12 @@
+const mysql = require('mysql2/promise');
+
+const mysqlConfig = {
+  host: process.env.MYSQL_HOST || 'localhost',
+  user: process.env.MYSQL_USER || 'root',
+  password: process.env.MYSQL_PASSWORD || 'root',
+  database: process.env.MYSQL_DB || 'mydb'
+}
+
+const connection = mysql.createPool(mysqlConfig);
+
+module.exports = connection;

12eac46163ad40830b9098be2f869c697288b648
Rafael <rafaelmagalhaes1@live.com>
2023-12-10 20:57:32 -0300

    feat: create end point /users

--- backend/src/app.js
+++ backend/src/app.js
@@ -1,11 +1,16 @@
 const express = require('express');
-const jwt = require('jsonwebtoken');
+const connection = require('./db/connection');
 
 const app = express();
 app.use(express.json());
 
 app.get('/', (_req, res) => {
-  res.send('Hello world');
+  res.send('Home Page!');
+});
+
+app.get('/users', async (_req, res) => {
+  const [users] = await connection.execute('SELECT * FROM users');
+  res.send(users);
 });
 
 module.exports = app;
